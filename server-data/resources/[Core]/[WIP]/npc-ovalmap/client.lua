e27a3bc458292c90faceecb661bd4e0164db503c
IcmpOff <71145797+Icmp@users.noreply.github.com>
2021-06-25 18:22:09 +0100

    Update client.lua

--- server-data/resources/[Core]/[WIP]/npc-ovalmap/client.lua
+++ server-data/resources/[Core]/[WIP]/npc-ovalmap/client.lua
@@ -4,31 +4,34 @@ posY = 0.00-- 0.0152
 width = 0.200
 height = 0.28 --0.354
 
-Citizen.CreateThread(function()
-	RequestStreamedTextureDict("circlemap", false)
-	while not HasStreamedTextureDictLoaded("circlemap") do
-		Wait(100)
-	end
+Citizen.CreateThread(
+    function()
+        RequestStreamedTextureDict("circlemap", false)
+        while not HasStreamedTextureDictLoaded("circlemap") do
+            Wait(100)
+        end
 
-	AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
+        AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "circlemap", "radarmasksm")
 
-	SetMinimapClipType(1)
-	SetMinimapComponentPosition('minimap', 'L', 'B', posX, posY, width, height)
-	-- SetMinimapComponentPosition('minimap_mask', 'L', 'B', 0.0, 0.032, 0.101, 0.259)
-	SetMinimapComponentPosition('minimap_mask', 'L', 'B', posX, posY, width, height)
-	SetMinimapComponentPosition('minimap_blur', 'L', 'B', -0.01, 0.024, 0.256, 0.337)
+        SetMinimapClipType(1)
+        SetMinimapComponentPosition("minimap", "L", "B", 0.025, -0.03, 0.153)
+        SetMinimapComponentPosition("minimap_mask", "L", "B", 0.135, 0.12, 0.093, 0.164)
+        SetMinimapComponentPosition("minimap_blur", "L", "B", 0.012, 0.022, 0.256, 0.337)
 
-    local minimap = RequestScaleformMovie("minimap")
-    SetRadarBigmapEnabled(true, false)
-    Wait(0)
-    SetRadarBigmapEnabled(false, false)
+        local minimap = RequestScaleformMovie("minimap")
 
-    while true do
+        SetRadarBigmapEnabled(true, false)
+        Citizen.Wait(100)
+        SetRadarBigmapEnabled(false, false)
+
+        Citizen.Wait(1000)
+
+    --[[while true do
         Wait(0)
         BeginScaleformMovieMethod(minimap, "SETUP_HEALTH_ARMOUR")
         ScaleformMovieMethodAddParamInt(3)
         EndScaleformMovieMethod()
-    end
+    end]]--
 end)
 
 local isPause = false
@@ -53,6 +56,60 @@ Citizen.CreateThread(function()
     end
 end)
 
+Citizen.CreateThread(
+    function()
+        while true do
+
+            Citizen.Wait(VitalsUpdateInterval)
+
+            local ped = PlayerPedId()
+            local vehicle = GetVehiclePedIsIn(ped)
+            local health = GetEntityHealth(ped) - 100
+            local serverid = GetPlayerServerId(PlayerId())
+            local pauseMenu = IsPauseMenuActive()
+
+           if pauseMenu and not UIHidden then
+                 SendNUIMessage(
+                        {
+                            type = "hideUI"
+                        }
+                    )
+                 UIHidden = true
+            elseif UIHidden and not pauseMenu then
+                 SendNUIMessage(
+                        {
+                            type = "showUI"
+                        }
+                    )
+                UIHidden = false
+            end
+
+            if not Config.AlwaysDisplayRadar then
+                if vehicle ~= 0 and UIRadar then
+                    SendNUIMessage(
+                        {
+                            type = "openMapUI"
+                        }
+                    )
+                    DisplayRadar(true)
+                    UIRadar = false
+                elseif not UIRadar and vehicle == 0 then
+                    SendNUIMessage(
+                        {
+                            type = "closeMapUI"
+                        }
+                    )
+                    UIRadar = true
+                    DisplayRadar(false)
+                end
+            else
+                DisplayRadar(true)
+            end
+
+            
+        end
+    end
+)
 
 -- local uiHidden = false
 
