1050c864617f2b2db691c22a66353fb463fda20f
OGN <79502719+Jigara@users.noreply.github.com>
2021-06-27 10:54:58 -0400

    Broken Base
    To be replaced with the updated framework core.

--- server-data/resources/[Core]/[WIP]/npc-base/server/sv_commands.lua
+++
@@ -1,6 +0,0 @@
-NPC.Commands = NPC.Commands or {}
-NPC.Commands.Registered = NPC.Commands.Registered or {}
-
-AddEventHandler("npc-base:exportsReady", function()
-    addModule("Commands", NPC.Commands)
-end)
