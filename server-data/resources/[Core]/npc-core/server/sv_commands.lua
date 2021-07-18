640c19f3761548e79bf7fdd187ca43164759b197
OGN <79502719+Jigara@users.noreply.github.com>
2021-07-18 14:42:33 -0400

    Fresh Core
    NPCore Collective.

--- server-data/resources/[Core]/npc-core/server/sv_commands.lua
+++
@@ -1,6 +0,0 @@
-NPC.Commands = NPC.Commands or {}
-NPC.Commands.Registered = NPC.Commands.Registered or {}
-
-AddEventHandler("npc-core:exportsReady", function()
-    addModule("Commands", NPC.Commands)
-end)
